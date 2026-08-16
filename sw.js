1d4afd99c85ea697d6183efa03989610ec1e00e8
brunodiasef <brunopersonalsgp@gmail.com>
2026-08-16 14:03:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    interceptadas: sempre vão direto para a rede, pois os dados precisam
    estar sempre atualizados. */
 
-const CACHE_NAME = 'monitoramento-aee-v4';
+const CACHE_NAME = 'monitoramento-aee-v5';
 const APP_SHELL = [
   './',
   './index.html',
